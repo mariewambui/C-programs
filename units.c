c7f3418ae7f47c692e9e04ab319b7f6bb598c80e
mariewambui <mariewambui2662025@gmail.com>
2025-09-19 16:26:33 +0300

    Add files via upload

---
+++ units.c
@@ -0,0 +1,21 @@
+/*
+Name:Mary Wambui 
+Reg:CT101/G/26514/25
+Description:units
+*/
+
+#include<stdio.h>
+
+int main()
+{
+    printf("UNITS\n");
+    printf("1.Physics\n");
+    printf("2.Introduction to computer architecture \n");
+    printf("3.Physics\n ");
+    printf("4.Analogue electronics \n");
+    printf("5.Descrete Mathematics\n");
+    printf("6.Mathematics for science \n");
+    printf("7.Communication skills\n"); 
+    printf("8.Fundementals of ICT");   
+    
+} 
