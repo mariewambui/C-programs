689715919899b9a121424cfea2dc38e506bf4017
mariewambui <mariewambui2662025@gmail.com>
2025-09-25 23:22:11 +0300

    Add files via upload

---
+++ compound interest.c
@@ -0,0 +1,29 @@
+/*
+Name:Mary Wambui 
+Reg no.: CT101/G/26514/25
+Description :Compouinterest 
+*/
+#include <stdio.h>
+  int main()
+  {
+  	float principle,rate,time,compounding_years,compound_intrest,final_amount;
+  	printf("enter princple:");
+  	scanf("%f" ,&principle);
+  		
+  	printf("enter rate:");
+  	scanf("%f" ,&rate);
+  	
+  	printf("enter time:");
+  	scanf("%f" ,&time);
+  		
+  	printf("enter compounding_years:");
+  	scanf("%f" ,&compounding_years);
+  	
+  	final_amount = principle * pow((1+rate/100), time * compounding_years);
+  	
+  	compound_intrest =final_amount-principle;
+  	
+  	printf("your compound_intrest is ksh %.2f" ,compound_intrest);
+  	
+  	return 0;
+  }
