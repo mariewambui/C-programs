123bb232832c09cd6bb5468301c7461ea99caa8f
mariewambui <mariewambui2662025@gmail.com>
2025-10-07 20:00:13 +0300

    Add files via upload

---
+++ do_while_loop.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main () {
+
+//start,stop,step
+int i=10;
+do{
+printf("%d\t", i);
+i--;
+} while(i>=5);
+
+
+
+return 0;
+}
---
+++ for_loop.c
@@ -0,0 +1,20 @@
+/*
+Name:Mary Wambui 
+Reg no:CT101/G/26514/25
+Description :use_of_loop
+*/
+#include<stdio.h>
+
+int main()
+{
+    //(start;step;stop)()
+    //for(intilisation;condition;stop)
+    int i;
+    
+    for(i=100;i>=50;i--){
+    printf("%d\n",i);
+    
+    }
+    
+    return 0;
+}
