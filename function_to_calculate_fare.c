5f0bae4bdd474b6ef68f22c3973ebe9a95a4b7af
mariewambui <mariewambui2662025@gmail.com>
2025-10-22 11:12:48 +0300

    Add files via upload

---
+++ function_to_calculate_fare.c
@@ -0,0 +1,25 @@
+/*
+Name : Kamau Mary Wambui 
+Reg no:CT101/G/26514/25
+Description : function to calculateFare
+*/
+#include <stdio.h>
+
+// Function to calculate fare
+int calculateFare(int distance) {
+    int fareRate = 50;   // fare rate per kilometer in KSh
+    int totalFare = distance * fareRate;
+    return totalFare;
+}
+
+int main() {
+    int distance;
+    
+    printf("Enter distance traveled (in km): ");
+    scanf("%d", &distance);
+    
+    int fare = calculateFare(distance);
+    printf("Total fare = KSh. %d\n", fare);
+    
+    return 0;
+}
